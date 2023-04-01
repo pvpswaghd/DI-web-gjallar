f1d5c443d89afce306243d1ddaab5f46fce33074
pvpswaghd <44018990+pvpswaghd@users.noreply.github.com>
2023-04-02 02:52:52 +0800

    basic updates

--- js/app.js
+++ js/app.js
@@ -1,27 +1,84 @@
-//AOS.init({duration: 500, once:true});
-const faders = document.querySelectorAll('.fade-in');
-const sliders = document.querySelectorAll('.slide-in');
-const appearOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px -100px 0px"
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
 };
-const appearOnScroll = new IntersectionObserver(
-    function(entries, appearOnScroll) {
-        entries.forEach(entry => {
-            if (!entry.isIntersecting){
-                return;
-            } else {
-                entry.target.classList.add("appear");
-                appearOnScroll.unobserve(entry.target);
-            }
-        })
-    }, 
-    appearOptions);
 
-faders.forEach(fader => {
-    appearOnScroll.observe(fader);
-});
+menu.addEventListener('click', mobileMenu);
 
-sliders.forEach(slider => {
-    appearOnScroll.observe(slider);
-})
+// // Show active menu when scrolling
+// const highlightMenu = () => {
+// const elem = document.querySelector('.highlight');
+// const homeMenu = document.querySelector('#home-page');
+// const aboutMenu = document.querySelector('#about-page');
+// const servicesMenu = document.querySelector('#services-page');
+// let scrollPos = window.scrollY;
+// console.log(scrollPos);
+
+//    // adds 'highlight' class to my menu items
+//    if (window.innerWidth > 960 && scrollPos < 600) {
+//      homeMenu.classList.add('highlight');
+//      aboutMenu.classList.remove('highlight');
+//      return;
+//    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+//      aboutMenu.classList.add('highlight');
+//      homeMenu.classList.remove('highlight');
+//      servicesMenu.classList.remove('highlight');
+//      return;
+//    } else if (window.innerWidth > 960 && scrollPos < 2345) {
+//      servicesMenu.classList.add('highlight');
+//      aboutMenu.classList.remove('highlight');
+//      return;
+//    }
+
+//    if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+//      elem.classList.remove('highlight');
+//    }
+//  };
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+//  Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector('.is-active');
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.remove('active');
+  }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
+
+
+
+// const faders = document.querySelectorAll('.fade-in');
+// const sliders = document.querySelectorAll('.slide-in');
+// const appearOptions = {
+//     threshold: 0,
+//     rootMargin: "0px 0px -100px 0px"
+// };
+// const appearOnScroll = new IntersectionObserver(
+//     function(entries, appearOnScroll) {
+//         entries.forEach(entry => {
+//             if (!entry.isIntersecting){
+//                 return;
+//             } else {
+//                 entry.target.classList.add("appear");
+//                 appearOnScroll.unobserve(entry.target);
+//             }
+//         })
+//     }, 
+//     appearOptions);
+
+// faders.forEach(fader => {
+//     appearOnScroll.observe(fader);
+// });
+
+// sliders.forEach(slider => {
+//     appearOnScroll.observe(slider);
+// })
